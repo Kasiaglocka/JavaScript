420a9dbcfda713e0d9308e8af81bca0d6a4ee1eb
Kasiaglocka <kasia.glocka@gmail.com>
2024-01-30 17:41:51 +0100

    zasady nazewnictwa zmiennych

---
+++ nazewnictwo.js
@@ -0,0 +1,20 @@
+//Poprawne nazwy zmiennych
+const zmienna1 = "Alicja";
+const $wynik = 24;
+const _rozmiar = 110;
+const $_nazwazmiennej = "Tomasz";
+
+//Niepoprawne nazwy zmiennych
+// const 1zmienna = "Wioletta";
+// const szerokosc&wysokosc = "120x90";
+// const pierwsza+druga = 12=75;
+// const nazwa.wynik = 78;
+
+//Wielkość liter
+const pierwsza = 1;
+const pierwszA = 2;
+
+//let const = "Kornelia";
+
+const pierwszeImieOsoby = "Karolina"; //Camel case
+const pierwsze_imie_osoby = "Michał";
