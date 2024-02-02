73b9204d623caae5242dbc9b37176a727084978c
Kasiaglocka <kasia.glocka@gmail.com>
2024-02-02 16:07:36 +0100

    zasięgi zmiennych wewnątrz funkcji

---
+++ zasiegi.js
@@ -0,0 +1,14 @@
+// function test() {
+//     console.log("TEST")
+// }
+  
+// test();
+
+const imie = "Dorota";
+
+function test() {
+    const nazwisko = "Kowalska";
+   console.log(imie + " " + nazwisko )
+}
+
+test();
