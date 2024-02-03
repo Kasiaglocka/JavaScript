72dad967209b02f6b8b8e3ac0efe42c92cb9e025
Kasiaglocka <kasia.glocka@gmail.com>
2024-02-03 22:32:33 +0100

    operatory przypisania w js

---
+++ operatory-przypisania.js
@@ -0,0 +1,16 @@
+//Standardowe przypisanie wartości
+const imie = "Bartosz";
+console.log(imie);
+
+let wiek = 13;
+wiek+=2; // => wiek = wiek+2
+console.log(wiek);
+
+wiek-=2; // => wiek = wiek-2
+console.log(wiek);
+
+wiek**2; // => wiek = wiek do potegi 2
+console.log(wiek);
+
+wiek/=2; // => wiek = wiek:2
+console.log(wiek);
