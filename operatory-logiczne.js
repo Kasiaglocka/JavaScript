f0783f3c555978b6dc594313be35f65938f12ce2
Kasiaglocka <kasia.glocka@gmail.com>
2024-02-05 19:07:42 +0100

    operatory logiczne w js

---
+++ operatory-logiczne.js
@@ -0,0 +1,18 @@
+const imie = "Kamil";
+const samochod = "BMW";
+
+//logiczny "i"
+//if(imie == "Kamil" && samochod == "BMW" ) {
+//    console.log("test");
+//}
+
+//logiczny "lub"
+//if(imie == "Kamil" || samochod == "BMW" ) {
+//    console.log("test");
+//}
+
+//logiczne "nie"
+let isOpen = true;
+isOpen = !isOpen;
+
+console.log(isOpen) //otrzymamy wartość false - negacja wszystko odwraca
