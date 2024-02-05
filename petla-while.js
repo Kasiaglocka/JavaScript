45fc9af8e8ecc584ceef56fb1510608730389044
Kasiaglocka <kasia.glocka@gmail.com>
2024-02-05 20:25:19 +0100

    pętla while w js

---
+++ petla-while.js
@@ -0,0 +1,19 @@
+//let licznik = 0;
+
+//while(licznik <= 5) {
+ //   console.log("TEST");
+  //  licznik++;
+//}
+
+let licznik = 0;
+const imionaOsob = ["Rafał", "Anna", "Robert", "Julia"]
+console.log(imionaOsob.length)
+
+//while (licznik < imionaOsob.length) {
+    //console.log(imionaOsob[licznik]);
+    //licznik++;
+//}
+while(imionaOsob[licznik] !== "Robert") {
+    console.log(imionaOsob[licznik]);
+    licznik++;
+}
