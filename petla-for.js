a76d72065b4a27fab87f6eebef0824749dc1e312
Kasiaglocka <kasia.glocka@gmail.com>
2024-02-05 19:57:59 +0100

    petla for w js

---
+++ petla-for.js
@@ -0,0 +1,7 @@
+//for(wartosc od jakiej ma liczyc; do jakiej wartosci; dekrementacja/inkrementacja petli)
+
+let licznik = 0;
+
+for(licznik; licznik<=5; licznik++) {
+    console.log("Licznik: " + licznik)
+}
