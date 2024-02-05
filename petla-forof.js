a6906dbed90054511f69ef6910855fc100da6c14
Kasiaglocka <kasia.glocka@gmail.com>
2024-02-05 21:03:40 +0100

    pętla for of

---
+++ petla-forof.js
@@ -0,0 +1,10 @@
+const liczby = [1,6,1,7];
+const imionaOsob = ["Kornelia", "Tomasz", "Karolina", "Michał"]
+
+//for (let value of liczby) {
+  //  console.log(value * 52)
+//}
+
+for (let value of imionaOsob) {
+    console.log(value + " Witaj użytkowniku!!")
+}
