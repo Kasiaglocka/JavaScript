3260db99c3fb2c3c86c33d367ca496e8fd9293e5
Kasiaglocka <kasia.glocka@gmail.com>
2024-02-06 18:56:58 +0100

    deklarowanie tablic

---
+++ tablice.js
@@ -0,0 +1,14 @@
+//Sposoby deklarowania tablic
+//const tablica1 =[];
+//console.log(tablica1.length);
+//console.log(typeof(tablica1))
+
+const tablica1 =  ["Natalia"];
+tablica1.push(2)
+console.log(tablica1)
+
+const tablica2 = newArray(5);
+console.log(tablica2[1])
+
+tablica2[3] = "Kamil"
+console.log(tablica2)
