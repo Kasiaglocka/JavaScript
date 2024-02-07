115ab7e3e0166de6eff2b3abc5ff68bbf1db285a
Kasiaglocka <kasia.glocka@gmail.com>
2024-02-07 16:29:57 +0100

    metoda for each w js

---
+++ metoda-foreach.js
@@ -0,0 +1,18 @@
+const imiona = ["Alicja", "Tomasz", "Agnieszka", "Beata", "Rafał", "Klaudiusz"]
+const liczby = [24, 63, 41, 42, 68, 85];
+const pustaTablica = [];
+
+//iona.forEach(imie => {
+//  console.log(imie)
+//
+console.log(
+ pustaTablica.map(item => {
+    return item;
+ })
+)
+
+console.log(
+    pustaTablica.forEach(item =>{
+        return item;
+    })
+)
