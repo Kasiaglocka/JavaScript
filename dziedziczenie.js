48a0ad7803c89ad5e1f319890b569bd27a69aa09
Kasiaglocka <kasia.glocka@gmail.com>
2024-02-15 12:40:45 +0100

    Dziedziczenie w js

---
+++ dziedziczenie.js
@@ -0,0 +1,14 @@
+class DrugaKlasa {
+    druga() {
+        return "Druga klasa"
+    }
+}
+
+class PierwszaKlasa {
+    pierwsza() {
+        return "Pierwsza klasa"
+    }
+}
+
+const obiekt1 = new PierwszaKlasa();
+const obiekt2 = new DrugaKlasa();
