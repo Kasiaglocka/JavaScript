05fb2d39b0ce9ac8049537dab62878d71fc78b1d
Kasiaglocka <kasia.glocka@gmail.com>
2024-02-20 21:51:05 +0100

    przypisanie funkcjonalności dla Entera

--- lista_zakupow/lista.js
+++ lista_zakupow/lista.js
@@ -1,19 +1,36 @@
-const produkt = document.querySelector("#add_input")
-const nazwaProduktu = produkt.getAttribute("value")
-const nowyProdukt = document.createElement("li")
-nowyProdukt.className = "items"
-nowyProdukt.innerHTML = nazwaProduktu
-
-const nowyButton = document.createElement("button")
-nowyButton.className = "del_btn"
-nowyButton.innerHTML = "Usuń"
-
-nowyProdukt.appendChild(nowyButton);
-
-const staryUl = document.querySelector("#items_list")
-
-staryUl.appendChild(nowyProdukt)
 
 
 
-console.log(produkt.getAttribute("value"))
+const inputName = document.querySelector("#add_input")
+
+inputName.addEventListener("keydown", (e) =>{
+    if (e.key === `Enter`) {
+        console.log("Uruchomiono zdarzenie KEYDOWN!!!")
+        dodajProduktDoListy();
+    } 
+    
+})
+
+
+
+function dodajProduktDoListy() {
+    const produkt = document.querySelector("#add_input")
+    //const nazwaProduktu = produkt.getAttribute("value")
+    const nazwaProduktu = produkt.value
+    if (nazwaProduktu != null && nazwaProduktu != "") {
+        const nowyProdukt = document.createElement("li")
+        nowyProdukt.className = "items"
+        nowyProdukt.innerHTML = nazwaProduktu
+
+        const nowyButton = document.createElement("button")
+        nowyButton.className = "del_btn"
+        nowyButton.innerHTML = "Usuń"
+
+        nowyProdukt.appendChild(nowyButton);
+
+        const staryUl = document.querySelector("#items_list")
+
+        staryUl.appendChild(nowyProdukt)
+    }
+}
+
