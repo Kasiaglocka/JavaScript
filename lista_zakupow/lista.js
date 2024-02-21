904af529c8ce511bad81b5aaf947a0394054b931
Kasiaglocka <kasia.glocka@gmail.com>
2024-02-21 19:16:49 +0100

    Dodanie funkcjonalności usuwania produktów z listy

--- lista_zakupow/lista.js
+++ lista_zakupow/lista.js
@@ -1,4 +1,4 @@
-
+var identyfikator = 1
 
 
 const inputName = document.querySelector("#add_input")
@@ -21,16 +21,29 @@ function dodajProduktDoListy() {
         const nowyProdukt = document.createElement("li")
         nowyProdukt.className = "items"
         nowyProdukt.innerHTML = nazwaProduktu
-
+        nowyProdukt.id = identyfikator
+        
         const nowyButton = document.createElement("button")
         nowyButton.className = "del_btn"
         nowyButton.innerHTML = "Usuń"
+        nowyButton.value = identyfikator
+        identyfikator++
+        nowyButton.onclick = function(zdarzenie){
+            //console.log(zdarzenie.target.value)
+            usunProduktZListy(zdarzenie.target.value)
+        }
 
         nowyProdukt.appendChild(nowyButton);
 
         const staryUl = document.querySelector("#items_list")
 
         staryUl.appendChild(nowyProdukt)
+        produkt.value = ""
     }
 }
 
+function usunProduktZListy(idElementuDoUsuniecia) {
+    const naszaLista = document.querySelector("#items_list")
+    const elementDoUsuniecia = document.getElementById(idElementuDoUsuniecia)
+    naszaLista.removeChild(elementDoUsuniecia)
+}
